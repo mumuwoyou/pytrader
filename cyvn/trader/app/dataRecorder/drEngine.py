1ca13da7cc3b8fc40c211cdb00449099f552ba68
mumuwoyou <mumuwoyou@msn.com>
2018-12-03 08:32:58 +0800

    update

--- cyvn/trader/app/dataRecorder/drEngine.py
+++ cyvn/trader/app/dataRecorder/drEngine.py
@@ -520,18 +520,14 @@ class RecorderBarManager(object):
             if not self.bar:
                 dt = tick.datetime.replace(second=0, microsecond=0)  # 将秒和微秒设为0
                 if (dt != datetime.strptime(' '.join([tick.date, '09:00:00.000']), '%Y%m%d %H:%M:%S.%f')
-                    or dt != datetime.strptime(' '.join([tick.date, '10:30:00.000']), '%Y%m%d %H:%M:%S.%f')
-                    or dt != datetime.strptime(' '.join([tick.date, '13:30:00.000']), '%Y%m%d %H:%M:%S.%f')
                     or dt != datetime.strptime(' '.join([tick.date, '21:00:00.000']), '%Y%m%d %H:%M:%S.%f')):
                     self.bar = VtBarData()
                     newMinute = True
             # 新的一分钟
             elif self.bar.datetime.minute != tick.datetime.minute:
                 dt = tick.datetime.replace(second=0, microsecond=0)   # 将秒和微秒设为0
-                if (dt != datetime.strptime(' '.join([tick.date, '09:00:00.000']), '%Y%m%d %H:%M:%S.%f')
-                    or dt != datetime.strptime(' '.join([tick.date, '10:30:00.000']), '%Y%m%d %H:%M:%S.%f')
-                    or dt != datetime.strptime(' '.join([tick.date, '13:30:00.000']), '%Y%m%d %H:%M:%S.%f')
-                    or dt != datetime.strptime(' '.join([tick.date, '21:00:00.000']), '%Y%m%d %H:%M:%S.%f')):
+                if (dt != datetime.strptime(' '.join([tick.date, '10:30:00.000']), '%Y%m%d %H:%M:%S.%f')
+                    or dt != datetime.strptime(' '.join([tick.date, '13:30:00.000']), '%Y%m%d %H:%M:%S.%f')):
                     # 生成上一分钟K线的时间戳
                     self.bar.datetime = tick.datetime
                     self.bar.datetime = self.bar.datetime.replace(second=0, microsecond=0)  # 将秒和微秒设为0
